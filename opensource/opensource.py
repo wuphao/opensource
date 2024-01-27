fdc46c3ff5b9718d3c43729a634dae8b1babd302
wph <614249290@qq.com>
2024-01-27 12:04:04 +0800

    second commit

--- opensource/opensource.py
+++ opensource/opensource.py
@@ -26,38 +26,3 @@ df = pd.DataFrame(result, columns=['name', 'start', 'address', 'description'])
 
 df.to_excel('data.xlsx', index=False)
 
-# import pygal
-# from pygal.style import LightColorizedStyle as LCS, LightenStyle as LS
-# names, plot_dicts = [], []
-# for repo_dict in result:
-#     value = int(repo_dict[1][0])*1000+int(repo_dict[1][2])*100
-#     names.append(repo_dict[0])
-#     description = repo_dict[3]
-#     if not description:
-#         description = "No description provided."
-#
-#     plot_dict = {
-#         'value': value,
-#         'label': description,
-#         'xlink': repo_dict[2],
-#         }
-#     plot_dicts.append(plot_dict)
-# my_style = LS('#333366', base_style=LCS)
-# my_style.title_font_size = 24
-# my_style.label_font_size = 14
-# my_style.major_label_font_size = 18
-#
-# my_config = pygal.Config()
-# my_config.x_label_rotation = 45
-# my_config.show_legend = False
-# my_config.truncate_label = 15
-# my_config.show_y_guides = False
-# my_config.width = 1000
-#
-# chart = pygal.Bar(my_config, style=my_style)
-# chart.title = 'Most-Starred Python Projects on Gitee'
-# chart.x_labels = names
-#
-# chart.add('', plot_dicts)
-# chart.render_to_file('python_repos.svg')
-
